8f32b23172777ac1e2f292a259a74ca352a5d320
Juliana <89608026+juliana-barreto@users.noreply.github.com>
2026-08-09 08:34:34 -0300

    feat: implement prorated invoice amount calculation in BillingService

--- src/main/java/com/juliana_barreto/saas_billing_engine/modules/invoice/services/BillingService.java
+++ src/main/java/com/juliana_barreto/saas_billing_engine/modules/invoice/services/BillingService.java
@@ -5,7 +5,10 @@ import com.juliana_barreto.saas_billing_engine.modules.invoice.enums.InvoiceStat
 import com.juliana_barreto.saas_billing_engine.modules.invoice.repositories.InvoiceRepository;
 import com.juliana_barreto.saas_billing_engine.modules.subscription.entities.Subscription;
 import com.juliana_barreto.saas_billing_engine.modules.subscription.repositories.SubscriptionRepository;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -22,13 +25,19 @@ public class BillingService {
   public void generateInvoice(Subscription subscription, LocalDate billingDate) {
     Invoice invoice = new Invoice();
     invoice.setSubscription(subscription);
-    invoice.setPeriodStart(subscription.getNextBillingDate());
     
-    LocalDate periodEnd = subscription.getNextBillingDate()
-        .plusDays(subscription.getCurrentPlan().getBillingCycleDays());
+    LocalDate periodStart = subscription.getNextBillingDate();
+    invoice.setPeriodStart(periodStart);
+    
+    int cycleDays = subscription.getCurrentPlan().getBillingCycleDays();
+    LocalDate periodEnd = periodStart.plusDays(cycleDays);
     invoice.setPeriodEnd(periodEnd);
     
-    invoice.setAmount(subscription.getCurrentPlan().getPrice());
+    BigDecimal planPrice = subscription.getCurrentPlan().getPrice();
+    long actualDays = ChronoUnit.DAYS.between(periodStart, periodEnd);
+    BigDecimal amount = calculateProratedAmount(planPrice, cycleDays, actualDays);
+    
+    invoice.setAmount(amount);
     invoice.setDueDate(billingDate.plusDays(5));
     invoice.setStatus(InvoiceStatus.PENDING);
     
@@ -36,5 +45,13 @@ public class BillingService {
     
     subscription.setNextBillingDate(periodEnd);
     subscriptionRepository.save(subscription);
+  }
+
+  private BigDecimal calculateProratedAmount(BigDecimal fullPrice, int cycleDays, long actualDays) {
+    if (actualDays == cycleDays) {
+      return fullPrice;
+    }
+    BigDecimal dailyRate = fullPrice.divide(BigDecimal.valueOf(cycleDays), 4, RoundingMode.HALF_UP);
+    return dailyRate.multiply(BigDecimal.valueOf(actualDays)).setScale(2, RoundingMode.HALF_UP);
   }
 }
