8c8efc8ce36d4b089ef99b7e68afb8077c2def64
Juliana <89608026+juliana-barreto@users.noreply.github.com>
2026-08-09 08:20:14 -0300

    feat: add unit tests for Billing Engine application context

---
+++ src/test/java/com/juliana_barreto/saas_billing_engine/BillingEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juliana_barreto.saas_billing_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
